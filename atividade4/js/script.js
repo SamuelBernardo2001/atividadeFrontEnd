b86aceaef91de41c1fc2fa13477c5e90ca08cdde
SamuelBernardo2001 <samuel.20241000151@aluno.alpha.edu.br>
2025-10-01 18:26:36 -0300

    iniciando a function para alterar o tema

--- atividade4/js/script.js
+++ atividade4/js/script.js
@@ -3,3 +3,6 @@ const imagemTema = document.getElementById('imagem-tema'); // Seleciona a imagem
 const imagemSol = 'https://img.icons8.com/color/96/000000/sun.png'; // URL da imagem do sol
 const imagemLua = 'https://img.icons8.com/color/96/000000/moon.png'; // URL da imagem da lua
 
+function alterarTema() {
+
+}
