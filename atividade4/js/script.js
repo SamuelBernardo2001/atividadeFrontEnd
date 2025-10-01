8be33f04f5232d23353583f5c8e80cbd1e277453
SamuelBernardo2001 <samuel.20241000151@aluno.alpha.edu.br>
2025-10-01 18:25:55 -0300

    criando uma const para seleciona o tema da imagem, como tbm foi criado as url da imagem sol e lua

---
+++ atividade4/js/script.js
@@ -0,0 +1,5 @@
+const imagemTema = document.getElementById('imagem-tema'); // Seleciona a imagem do tema
+
+const imagemSol = 'https://img.icons8.com/color/96/000000/sun.png'; // URL da imagem do sol
+const imagemLua = 'https://img.icons8.com/color/96/000000/moon.png'; // URL da imagem da lua
+
