9b4f60321ceab66ca12e82186242f34f9cc3f4ed
SamuelBernardo2001 <samuel.20241000151@aluno.alpha.edu.br>
2025-10-01 18:29:17 -0300

    criando a const body para alterar a classe d tema para fazer a alteração do sol e lua

--- atividade4/js/script.js
+++ atividade4/js/script.js
@@ -1,7 +1,9 @@
 const imagemTema = document.getElementById('imagem-tema'); // Seleciona a imagem do tema
+const body = document.body; // Seleciona o corpo do documento
 
 const imagemSol = 'https://img.icons8.com/color/96/000000/sun.png'; // URL da imagem do sol
 const imagemLua = 'https://img.icons8.com/color/96/000000/moon.png'; // URL da imagem da lua
+
 
 function alterarTema() {
 
