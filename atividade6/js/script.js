9afc4c17a3478c05c09ab51e75a4ececf84cc438
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-10-04 11:51:02 -0300

    criando o metodo para sortear numeros aleatorios

--- atividade6/js/script.js
+++ atividade6/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('sorteio');
     const botaoSorteio = document.getElementById('botao-sorteio'); 
     const resultado = document.getElementById('resultado');
+
+    function sortearNumeros(min, max,qtd){
+        
+    }
 });
