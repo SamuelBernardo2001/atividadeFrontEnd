564ab40a56b2b2dffa6ee02085b80e701ab59f66
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-10-04 17:32:33 -0300

    criando uma função para converte as geraçoes para numeros pos quando ser coloca a geração precisa coloca o generation-i ent criei a função para a conversao numerica

--- atividade7/js/script.js
+++ atividade7/js/script.js
@@ -93,9 +93,24 @@ document.addEventListener('DOMContentLoaded', () => {
              msgGeracao.textContent = "✅ Acertou a geração!";  // Mensagem de acerto
             etapaTipo.classList.remove("hidden"); // Libera próxima etapa
             }else {
-                 msgGeracao.textContent = `❌ Geração errada! Era ${geracaoPokemon}`;
+                 msgGeracao.textContent = `❌ Geração errada! Era ${geracaoPokemon}`; 
     }
     });
+
+
+function converterGeracao(g) {
+  const map = {
+    "generation-i": "1",
+    "generation-ii": "2",
+    "generation-iii": "3",
+    "generation-iv": "4",
+    "generation-v": "5",
+    "generation-vi": "6",
+    "generation-vii": "7",
+    "generation-viii": "8",
+    "generation-ix": "9"
+  };
+  return map[g] || g;}
 
 
 });
