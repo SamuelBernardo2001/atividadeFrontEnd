e21b4e1bb1c93b4789f026dd32852f89e604193c
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-10-04 11:54:23 -0300

    criando um while para continua rodando ate atingir a quantidade desejada

--- atividade6/js/script.js
+++ atividade6/js/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('sorteio');
-    const botaoSorteio = document.getElementById('botao-sorteio'); 
-    const resultado = document.getElementById('resultado');
+    const form = document.getElementById('sorteio');    // Seleciona o formulário
+    const botaoSorteio = document.getElementById('botao-sorteio');  // Seleciona o botão de sorteio
+    const resultado = document.getElementById('resultado');     // Seleciona o parágrafo para exibir o resultado
 
     function sortearNumeros(min, max,qtd){
-        const numerosSorteados = new Set();
+        const numerosSorteados = new Set(); // Usando Set para evitar duplicatas
+        while(numerosSorteados.size < qtd){
+            
+        }
     }
 });
