cde4b9efb237c87c4d2335656fa95e0714383532
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-10-04 14:27:48 -0300

    foi criado todos os const para seleciona cada  formulario do html

--- atividade7/js/script.js
+++ atividade7/js/script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const botaoPokemon = document.getElementById('botao-pokemon');
+    const botaoPokemon = document.getElementById('botao-pokemon');  // Seleciona o botão
+    const nomePokemon = document.getElementById('nome-pokemon');
+    const imagemPokemon = document.getElementById('imagem-pokemon');
+
+    const etapaNome = document.getElementById("etapas-pokemon-nome");
+    const etapaGeracao = document.getElementById("etapa-pokemon-geracao");
+    const etapaTipo = document.getElementById("etapa-pokemon-tipo");
+
+    const inputNome = document.getElementById("resposta-nome");
+    const inputGeracao = document.getElementById("resposta-geracao");
+    const inputTipo = document.getElementById("resposta-tipo");
+
+    const msgNome = document.getElementById("mensagem-nome");
+    const msgGeracao = document.getElementById("mensagem-geracao");
+    const msgTipo = document.getElementById("mensagem-tipo");
 });
