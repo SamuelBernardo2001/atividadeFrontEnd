ebb4920fd8829ef51dd2769ad5d7cb967d80f717
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-10-04 10:54:12 -0300

    criando um elemento para verifica o tamanho do textarea vericando o valor e o tamanho

---
+++ atividade5/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.getElementById('texto'); // Seleciona o TextArea
+    const contador = document.getElementById('contador');
+    const limite = 100;
+
+    function atualizarContador() {
+        const comprimentoAtual = textarea.value.length;
+    }
+});    // Garante que o DOM esteja carregado antes de executar o script
