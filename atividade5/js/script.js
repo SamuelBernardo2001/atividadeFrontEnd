66d830a191134d2be541a6ee73974f13d5bfd483
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-10-04 11:04:22 -0300

    deixando o contador Preto enquanto a opçãp for dentro do padrão ser nao exercedo o limite

--- atividade5/js/script.js
+++ atividade5/js/script.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const limite = 100;
 
     function atualizarContador() {
-        const comprimentoAtual = textarea.value.length;
+        const comprimentoAtual = textarea.value.length; // Obtém o comprimento atual do texto
+        contador.textContent = `Caracteres: ${comprimentoAtual} / ${limite}`; // Atualizar O Contador
+
+        if (comprimentoAtual > limite) {
+            contador.style.color = 'red'; // Muda a cor do contador para vermelho se o limite for excedido
+            textarea.value = textarea.value.substring(0, limite);   // Limita o texto ao máximo permitido
+        } else {
+            contador.style.color = 'black';
+        }
     }
 });    // Garante que o DOM esteja carregado antes de executar o script
