97b81d06db4777f2d3af753fa6b435fe0a843efb
SamuelBernado2001 <Samuel.20241000151@aluno.alpha.edu.br>
2025-10-04 11:49:39 -0300

    Selecionando o Sorteio

--- atividade6/js/script.js
+++ atividade6/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('sorteio');
-    const botaoSorteio = document.getElementById('botao-sorteio');
+    const botaoSorteio = document.getElementById('botao-sorteio'); 
+    const resultado = document.getElementById('resultado');
 });
